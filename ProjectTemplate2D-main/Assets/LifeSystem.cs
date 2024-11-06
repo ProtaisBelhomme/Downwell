2924e161a8324946e82ca42d9641c9f2563b14c0
valere <valerecuche@gmail.com>
2024-11-06 17:22:59 +0100

    [ADD] Life systheme

---
+++ ProjectTemplate2D-main/Assets/LifeSystem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LifeSystem : MonoBehaviour
+{
+    public int life = 0;
+    public int maxLife = 3;
+
+    public UnityEvent onDie;
+
+    private void Start()
+    {
+        FullHeal();
+    }
+
+    public bool CheckAlive()
+    {
+        return life > 0;
+    }
+    public void TakeDamage(int damage)
+    {
+        life -= damage;
+        life = Mathf.Clamp(life, 0, maxLife);
+
+        if(life <= 0 )
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        life = 0;
+        onDie?.Invoke();
+    }
+
+    public void Heal(int healValue)
+    {
+        life += healValue;
+        life = Mathf.Clamp(life, 0, maxLife);
+
+    }
+    public void FullHeal()
+    {
+        life = maxLife;
+    }
+}
